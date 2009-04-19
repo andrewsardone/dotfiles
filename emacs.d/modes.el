0f3f93d9539a2deff21e212cfa0bc0c2cf75209a
Andrew Sardone <andrew.sardone@gmail.com>
2009-04-19 19:09:46 -0400

    Added Markdwon mode. http://jblevins.org/projects/markdown-mode/

--- emacs.d/modes.el
+++ emacs.d/modes.el
@@ -1,1 +1,6 @@
+(autoload 'markdown-mode "markdown-mode.el"
+   "Major mode for editing Markdown files" t)
+(setq auto-mode-alist
+   (cons '("\\.md" . markdown-mode) auto-mode-alist))
+
 (provide 'modes)
