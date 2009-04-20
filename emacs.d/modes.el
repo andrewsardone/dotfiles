ce1338cf355fe510da332d174796120227dc121d
Andrew Sardone <andrew.sardone@gmail.com>
2009-04-20 00:43:07 -0400

    Added haml and sass major modes.

--- emacs.d/modes.el
+++ emacs.d/modes.el
@@ -9,4 +9,14 @@
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 
+; haml & sass
+(autoload 'haml-mode "haml-mode.el"
+   "haml mode for haml files" t)
+(setq auto-mode-alist
+   (cons '("\\.haml" . haml-mode) auto-mode-alist))
+(autoload 'sass-mode "sass-mode.el"
+   "sass mode for sass files" t)
+(setq auto-mode-alist
+   (cons '("\\.sass" . sass-mode) auto-mode-alist))
+
 (provide 'modes)
