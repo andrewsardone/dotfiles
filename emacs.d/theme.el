602b54a473eb65acdf8353698359bfafc5078386
Andrew Sardone <andrew.sardone@gmail.com>
2009-04-19 23:47:40 -0400

    Added the color-theme package: http://www.emacswiki.org/emacs/ColorTheme

--- emacs.d/theme.el
+++ emacs.d/theme.el
@@ -1,1 +1,19 @@
+(setq load-path (cons "~/.emacs.d/vendor/color-theme-6.6.0" load-path))
+(require 'color-theme)
+(color-theme-initialize)
+(setq color-theme-is-global t)
+
+;; (load "~/.emacs.d/vendor/color-theme-ir-black")
+;; (color-theme-ir-black)
+
+(setq mac-allow-anti-aliasing t)
+
+; window size and position
+(setq initial-frame-alist
+      '((left . 30) (top . 40)
+	(width . 135) (height . 60)))
+
+; font
+(set-face-font 'default "-apple-monaco-medium-r-normal--12-120-72-72-m-120-iso10646-1")
+
 (provide 'theme)
