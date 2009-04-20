bbe68ddb3ab9d3d7dffdb3ce5f44ee2a66459223
Andrew Sardone <andrew.sardone@gmail.com>
2009-04-19 23:25:58 -0400

    Added ELPA ruby-mode

--- emacs.d/modes.el
+++ emacs.d/modes.el
@@ -4,4 +4,9 @@
 (setq auto-mode-alist
    (cons '("\\.md" . markdown-mode) auto-mode-alist))
 
+; Ruby
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+
 (provide 'modes)
