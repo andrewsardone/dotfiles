bf1b901563c761ca00c61bb6fe1bf0a004a2c280
Andrew Sardone <andrew.sardone@gmail.com>
2009-04-23 12:11:33 -0400

    Don't mess with the whitespace.

--- emacs.d/global.el
+++ emacs.d/global.el
@@ -20,13 +20,15 @@
 (setq ring-bell-function 'ignore)
 
 ; show extra whitespace
-(setq show-trailing-whitespace t)
+; (setq show-trailing-whitespace t)
 
 ; delete trailing whitespace before save
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+; This can be a nuisance when working with a team, screwing
+; up your revision control commits.
+; (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ; ensure last line is a return
-(setq require-final-newline t)
+; (setq require-final-newline t)
 
 ; set encoding
 (prefer-coding-system 'utf-8)
