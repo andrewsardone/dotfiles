3218097ff1af86abc48ab30b6bbad7c2197f59d5
Andrew Sardone <andrew.sardone@gmail.com>
2009-04-24 07:44:37 -0400

    Installed magit-0.7, but it doesn't seem to work, i.e., it's not finding my git installation.

--- emacs.d/bindings.el
+++ emacs.d/bindings.el
@@ -9,4 +9,7 @@
 ;; Completion that uses many different methods to find options.
 (global-set-key (kbd "C-'") 'dabbrev-expand)
 
+;; Git status
+(global-set-key "\C-x\C-g" 'magit-status)
+
 (provide 'bindings)
