0846bfac8b1095da216e2c215302f754596eb1eb
Andrew Sardone <andrew.sardone@gmail.com>
2009-08-17 23:15:32 -0400

    Added some bin dirs to my environment PATH (for git accessiblity)

--- emacs.d/misc.el
+++ emacs.d/misc.el
@@ -3,4 +3,8 @@
      (set-face-foreground 'magit-diff-add "green3")
      (set-face-foreground 'magit-diff-del "red3")))
 
+(when (equal system-type 'darwin)
+  (setenv "PATH" (concat "/opt/local/bin:/usr/local/bin:" (getenv "PATH")))
+  (push "/opt/local/bin" exec-path))
+
 (provide 'misc)
