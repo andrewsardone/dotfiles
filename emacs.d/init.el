0cdb99b82926ad8ad12a32f78b6bf595f3381b5b
Andrew Sardone <andrew.sardone@gmail.com>
2009-12-25 01:57:18 -0500

    Cursor is now a yellow, blinking bar

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -137,17 +137,22 @@
 (when (equal system-type 'darwin)
   (setq mac-allow-anti-aliasing t))
 
-; window size and position
+;; window size and position
 (setq initial-frame-alist
       '((left . 30) (top . 24)
         (width . 145) (height . 50)))
 
-; font
-;; The Bitstream Vera Sans Mono font does not come installed
-;; To install it, visit http://ftp.gnome.org/pub/GNOME/sources/ttf-bitstream-vera/1.10/
-;; Otherwise, uncomment the set-face-font declaration below and comment out the bitstream one
-;; (set-face-font 'default "-apple-monaco-medium-r-normal--13-130-72-72-m-130-iso10646-1")
+;; font
+;;; The Bitstream Vera Sans Mono font does not come installed
+;;; To install it, visit http://ftp.gnome.org/pub/GNOME/sources/ttf-bitstream-vera/1.10/
+;;; Otherwise, uncomment the set-face-font declaration below and comment out the bitstream one
+;;; (set-face-font 'default "-apple-monaco-medium-r-normal--13-130-72-72-m-130-iso10646-1")
 (set-face-font 'default "-apple-Bitstream_Vera_Sans_Mono-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+
+;; cursor
+(setq-default cursor-type 'bar)
+(set-cursor-color "yellow")
+(blink-cursor-mode 1)
 
 ; plugins
 ;; anything
