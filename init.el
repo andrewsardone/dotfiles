ece7b68f98a6ac335f8bc409a1a311e18a3d7c4f
Andrew Sardone <andrew.sardone@gmail.com>
2010-01-12 22:16:13 -0500

    Bounce indenting in js2-mode for some smart indenting

--- init.el
+++ init.el
@@ -517,7 +517,8 @@ t)
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(js2-basic-offset 2))
+ '(js2-basic-offset 2)
+ '(js2-bounce-indent-p t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
