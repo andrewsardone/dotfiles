acc4c577bb9a1ea2af9dcfd2380e2e264e467034
Andrew Sardone <andrew.sardone@gmail.com>
2010-12-03 18:55:20 -0500

    Disabling prompts that get in my way

--- customizations/aps-keys.el
+++ customizations/aps-keys.el
@@ -4,6 +4,12 @@
 ; Make yes-or-no questions answerable with 'y' or 'n'
 (fset 'yes-or-no-p 'y-or-n-p)
 
+; disable confirmation if a file or buffer does not exist
+(setq confirm-nonexistent-file-or-buffer nil)
+
+; disable prompt that asks to create a new, non-existent buffer
+(setq ido-create-new-buffer 'always)
+
 ; backward-kill-word mapped to C-w (like bash)
 ; http://steve.yegge.googlepages.com/effective-emacs#item3
 (global-set-key "\C-w" 'backward-kill-word)
