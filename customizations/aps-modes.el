8ebaf5843947688e95c173477130e624da800032
Andrew Sardone <asardone@nutshell.com>
2010-12-15 20:22:05 -0500

    Helpful indent for return key in ruby-mode

--- customizations/aps-modes.el
+++ customizations/aps-modes.el
@@ -28,6 +28,9 @@
 (setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("Capfile" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.rake" . ruby-mode) auto-mode-alist))
+(eval-after-load 'ruby-mode
+  '(progn
+     (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)))
 
 ; RVM
 (require 'rvm)
