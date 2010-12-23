9d8c9ce9afbaa21e777e05d54c0872ad753c5c88
Andrew Sardone <andrew.sardone@gmail.com>
2010-12-23 17:43:07 -0500

    Utility functions for generating and inserting UUIDs

---
+++ utilities/uuid.el
@@ -0,0 +1,28 @@
+;; via Christopher Wellons
+;; http://nullprogram.com/blog/2010/05/11/
+
+;; 90aebf38-b33a-314b-1198-c9bffea2f2a2
+(defun uuid-create ()
+  "Return a newly generated UUID. This uses a simple hashing of variable data."
+  (let ((s (md5 (format "%s%s%s%s%s%s%s%s%s%s"
+                        (user-uid)
+                        (emacs-pid)
+                        (system-name)
+                        (user-full-name)
+                        user-mail-address
+                        (current-time)
+                        (emacs-uptime)
+                        (garbage-collect)
+                        (random)
+                        (recent-keys)))))
+    (format "%s-%s-3%s-%s-%s"
+            (substring s 0 8)
+            (substring s 8 12)
+            (substring s 13 16)
+            (substring s 16 20)
+            (substring s 20 32))))
+
+(defun uuid-insert ()
+  "Inserts a new UUID at the point."
+  (interactive)
+  (insert (uuid-create)))
