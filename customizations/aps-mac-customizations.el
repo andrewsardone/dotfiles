7ed66fa04c71abe0dfe96f8220afaeca0bdc6ae3
Andrew Sardone <andrew.sardone@gmail.com>
2010-12-24 15:07:39 -0500

    command-return keybinding to toggle fullscreen mode

--- customizations/aps-mac-customizations.el
+++ customizations/aps-mac-customizations.el
@@ -3,5 +3,6 @@
       (setq mac-allow-anti-aliasing t)
       (setq browse-url-browser-function 'browse-url-default-macosx-browser)
       (setq delete-by-moving-to-trash t)
-      (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")))
+      (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+      (global-set-key (kbd "s-<return>") 'ns-toggle-fullscreen)))
 
