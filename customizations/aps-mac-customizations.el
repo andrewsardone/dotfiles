a1d876e18b8a42508977a645ae35dfe947a491a2
Andrew Sardone <andrew.sardone@gmail.com>
2011-06-04 22:12:26 -0400

    Use Menlo 11 for default font

--- customizations/aps-mac-customizations.el
+++ customizations/aps-mac-customizations.el
@@ -3,7 +3,7 @@
       (setq mac-allow-anti-aliasing t)
       (setq browse-url-browser-function 'browse-url-default-macosx-browser)
       (setq delete-by-moving-to-trash t)
-      (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+      (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1")
       (global-set-key (kbd "s-<return>") 'ns-toggle-fullscreen)
       (global-set-key (kbd "C-z M-w") 'mac-pbcopy)
       (global-set-key (kbd "C-z C-y") 'mac-pbpaste)))
