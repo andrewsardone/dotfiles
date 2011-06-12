2903927188637ef4d7dd208659085cf363060952
Andrew Sardone <andrew.sardone@gmail.com>
2011-06-12 01:08:08 -0400

    Added NODE_PATH to my zsh paths file

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,5 @@
 export PATH=".:bin:$DOTFILES/bin:/usr/local/Cellar/python/2.7/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export NODE_PATH="/usr/local/lib/node:$NODE_PATH"
