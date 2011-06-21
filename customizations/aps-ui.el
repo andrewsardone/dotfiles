05ff44c7a8b7f6f87c5deac610b818185961b520
Andrew Sardone <asardone@nutshell.com>
2011-06-21 11:21:59 -0400

    diff and magit-diff colors closer to terminal colors

--- customizations/aps-ui.el
+++ customizations/aps-ui.el
@@ -53,19 +53,19 @@
 
 ; pretty diff-mode
 (custom-set-faces
- '(diff-added ((t (:foreground "green4"))))
+ '(diff-added ((t (:foreground "#b3fa86"))))
  '(diff-context ((t nil)))
  '(diff-file-header ((((class color) (min-colors 88) (background dark)) (:foreground "RoyalBlue1"))))
  '(diff-function ((t (:foreground "#00bbdd"))))
  '(diff-header ((((class color) (min-colors 88) (background dark)) (:foreground "RoyalBlue1"))))
- '(diff-hunk-header ((t (:foreground "#fbde2d"))))
+ '(diff-hunk-header ((t (:foreground "#d1d2fb"))))
  '(diff-nonexistent ((t (:inherit diff-file-header :strike-through nil))))
  '(diff-refine-change ((((class color) (min-colors 88) (background dark)) (:background "#182042"))))
- '(diff-removed ((t (:foreground "#red3")))))
+ '(diff-removed ((t (:foreground "#ff8277")))))
 
 ; pretty magit diffs (based on colors for diff-mode above)
-(set-face-attribute 'magit-diff-add nil :foreground "green4")
-(set-face-attribute 'magit-diff-del nil :foreground "red4")
+(set-face-attribute 'magit-diff-add nil :foreground "#b3fa86")
+(set-face-attribute 'magit-diff-del nil :foreground "#ff8277")
 (set-face-attribute 'magit-diff-file-header nil :foreground "RoyalBlue1")
-(set-face-attribute 'magit-diff-hunk-header nil :foreground "#fbde2d")
+(set-face-attribute 'magit-diff-hunk-header nil :foreground "#d1d2fb")
 (set-face-attribute 'magit-item-highlight nil :background "black")
