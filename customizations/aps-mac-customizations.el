d1989576743826d003e35447d7fca547c639c815
Andrew Sardone <andrew.sardone@gmail.com>
2011-09-06 12:03:35 -0400

    Don't open new files sent to emacs in a new frame
    I usually only want one frame.

--- customizations/aps-mac-customizations.el
+++ customizations/aps-mac-customizations.el
@@ -3,6 +3,7 @@
       (setq mac-allow-anti-aliasing t)
       (setq browse-url-browser-function 'browse-url-default-macosx-browser)
       (setq delete-by-moving-to-trash t)
+      (setq ns-pop-up-frames nil)
       (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1")
       (global-set-key (kbd "s-<return>") 'ns-toggle-fullscreen)
       (global-set-key (kbd "C-z M-w") 'mac-pbcopy)
