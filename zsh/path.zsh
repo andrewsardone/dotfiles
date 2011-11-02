6d694175e157e49e0f9ab406a5a70137b7d77c8e
Andrew Sardone <andrew.sardone@gmail.com>
2011-11-02 14:48:04 -0400

    Add local node_modules to PATH

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,4 +1,4 @@
-export PATH=".:bin:$DOTFILES/bin:/usr/local/Cellar/python/2.7/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH=".:bin:node_modules/.bin:$DOTFILES/bin:/usr/local/Cellar/python/2.7/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
