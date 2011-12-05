45c506d3416682b91fd6711475ec8e3933b69d8c
Andrew Sardone <andrew.sardone@gmail.com>
2011-12-05 08:48:20 -0500

    Return ido-use-filename-at-point to original nil value
    I couldn't get used to have ido guess the filename, making it hard to
    quickly jump to stuff upwards the file-system hierarchy. Maybe I'll
    enable this again at some point to give it another shot.

--- modules/aps-editor.el
+++ modules/aps-editor.el
@@ -40,7 +40,6 @@
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
       ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess
       ido-max-prospects 10
       ido-default-file-method 'selected-window)
 
