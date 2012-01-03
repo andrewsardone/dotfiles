b0fde2e51374d0a8a379b66b61958dabfba0dd97
Andrew Sardone <andrew.sardone@gmail.com>
2012-01-03 08:34:22 -0500

    Fix random indentation

--- modules/aps-ui.el
+++ modules/aps-ui.el
@@ -62,12 +62,12 @@
 
 ;; Mac customizations
 (if (string-equal system-type "darwin")
-    (progn
-      (setq mac-allow-anti-aliasing t)
-      (setq browse-url-browser-function 'browse-url-default-macosx-browser)
-      (setq delete-by-moving-to-trash t)
-      (setq ns-pop-up-frames nil)
-      (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1")
-      (global-set-key (kbd "s-<return>") 'ns-toggle-fullscreen)))
+   (progn
+     (setq mac-allow-anti-aliasing t)
+     (setq browse-url-browser-function 'browse-url-default-macosx-browser)
+     (setq delete-by-moving-to-trash t)
+     (setq ns-pop-up-frames nil)
+     (set-face-font 'default "-apple-Menlo-medium-normal-normal-*-11-*-*-*-m-0-iso10646-1")
+     (global-set-key (kbd "s-<return>") 'ns-toggle-fullscreen)))
 
 (provide 'aps-ui)
