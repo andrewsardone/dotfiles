26595e1c92fcdea2f8ab22eb90875ec175ac96d5
Andrew Sardone <andrew.sardone@gmail.com>
2012-01-03 08:33:57 -0500

    Customize diff colors

--- modules/aps-ui.el
+++ modules/aps-ui.el
@@ -40,6 +40,26 @@
   [unspecified "#616161" "#ff8277" "#b3fa86" "#fffdc9" "#a6d6fb"
    "#ff90f7" "#d1d2fb" "#f1f1f1"])
 
+(eval-after-load 'diff-mode
+  '(custom-set-faces
+    '(diff-added ((t (:foreground "#b3fa86"))))
+    '(diff-context ((t nil)))
+    '(diff-header ((((class color) (min-colors 88) (background dark)) (:foreground "RoyalBlue1"))))
+    '(diff-file-header ((((class color) (min-colors 88) (background dark)) (:foreground "RoyalBlue1"))))
+    '(diff-function ((t (:foreground "#00bbdd"))))
+    '(diff-hunk-header ((t (:foreground "#d1d2fb"))))
+    '(diff-nonexistent ((t (:inherit diff-file-header :strike-through nil))))
+    '(diff-refine-change ((((class color) (min-colors 88) (background dark)) (:background "#182042"))))
+    '(diff-removed ((t (:foreground "#ff8277"))))))
+
+(eval-after-load 'magit
+  '(custom-set-faces
+     '(magit-diff-add ((t :foreground "#b3fa86")))
+     '(magit-diff-del ((t :foreground "#ff8277")))
+     '(magit-diff-file-header ((t :foreground "RoyalBlue1")))
+     '(magit-diff-hunk-header ((t :foreground "#d1d2fb")))
+     '(magit-item-highlight ((t :background "black")))))
+
 ;; Mac customizations
 (if (string-equal system-type "darwin")
     (progn
