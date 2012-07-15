7b93d2e1c542d9e94df93f53fbe547a4eea04cfc
Andrew Sardone <andrew.sardone@gmail.com>
2012-07-15 11:21:25 -0400

    Simplify zsh configuration
    There is a lot of cruft in here, stolen from all over the place, much
    much of which I don't use, so I'm starting over.

--- zsh/env.zsh
+++
@@ -1,7 +0,0 @@
-export EDITOR=vim
-
-bindkey -e
-
-if [[ $TERM = "eterm-color" ]]; then
-   export TERM="xterm-color"
-fi
--- zsh/path.zsh
+++
@@ -1,5 +0,0 @@
-export PATH="bin:node_modules/.bin:$HOME/bin:/usr/local/Cellar/python/2.7/bin:/usr/local/bin:/usr/local/sbin:$PATH"
-
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-export NODE_PATH="/usr/local/lib/node_modules:$NODE_PATH"
