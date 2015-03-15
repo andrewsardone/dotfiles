21261f0291c369366471978b26d0aa0b28b1c3f3
Andrew Sardone <andrew@andrewsardone.com>
2015-03-15 14:18:20 -0400

    Add Mac fonts setup

---
+++ osx/fonts.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo ""
+echo "### Installing homebrew-cask fonts"
+echo ""
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# Check for Homebrew
+sh $DIR/install-homebrew.sh
+
+fonts=(
+  font-blokk
+  font-kreon
+  font-lato
+  font-open-sans
+  font-open-sans-condensed
+  font-pt-mono
+  font-pt-sans
+  font-pt-serif
+  font-roboto
+  font-source-code-pro
+)
+
+echo "installing cask..."
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+# Tap the fonts
+echo "installing caskroom/fonts"
+brew tap caskroom/fonts
+
+# install fonts
+echo "installing fonts..."
+brew cask install ${fonts[@]}
+
+brew cleanup
+
+exit 0
--- osx/index.sh
+++ osx/index.sh
@@ -7,3 +7,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 sh $DIR/osx-defaults
 sh $DIR/binaries.sh
 sh $DIR/apps.sh
+sh $DIR/fonts.sh
