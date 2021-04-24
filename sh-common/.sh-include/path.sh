ff68731a60f950ebdfe629a757b8c660d60efe1b
Andrew Sardone <andrew@andrewsardone.com>
2021-04-24 17:46:20 -0400

    Fix up toolbox to be on my $PATH

--- sh-common/.sh-include/path.sh
+++ sh-common/.sh-include/path.sh
@@ -1,7 +1,8 @@
 # path
 [ -z "$ANDROID_HOME" ] && export ANDROID_HOME=/usr/local/android-sdk/
-[ -z "$APS_PATH" ] && export APS_PATH="$HOME/.dotfiles/bin:/usr/local/bin:/usr/local/sbin:/usr/local/heroku/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools"
+[ -z "$APS_PATH" ] && export APS_PATH="$HOME/.dotfiles/bin:$HOME/.toolbox/bin:/usr/local/bin:/usr/local/sbin:/usr/local/heroku/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools"
 [ -z "$DEFAULT_PATH" ] && export DEFAULT_PATH=$PATH
+
 export PATH="$APS_PATH:$DEFAULT_PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
