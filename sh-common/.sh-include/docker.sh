51ae1aee6a12585f4c6c82c1fe16b83cf352f8ae
Andrew Sardone <andrew@andrewsardone.com>
2022-10-16 11:59:35 -0400

    Set default Docker daemon socket to Colima's location
    Since I switched to Colima over Docker for Mac[1], I need to direct the
    docker CLI to the location of its socket.
    
    Found via https://github.com/nektos/act/issues/1051#issuecomment-1070599087
    
    [1]: https://github.com/andrewsardone/dotfiles/commit/2375333

--- sh-common/.sh-include/docker.sh
+++ sh-common/.sh-include/docker.sh
@@ -2,3 +2,9 @@
 
 docker_func=$HOME/.dockerfunc
 [ -f $docker_func ] && source $docker_func
+
+# Colima
+# Set the default Docker daemon socket to Colima's location. I use Colima
+# instead of the Docker Desktop application.
+# https://github.com/nektos/act/issues/1051#issuecomment-1070599087
+export DOCKER_HOST="unix://$HOME/.colima/docker.sock"
