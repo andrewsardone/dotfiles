ed5676b2ed8ab089066319674a2e5509f968c30e
Andrew Sardone <andrew@andrewsardone.com>
2022-11-27 16:20:58 -0500

    Add folke/zen-mode Neovim dependency
    For a nicer and more focused writing experience.

--- xdg_base_directory/.config/nvim/lua/user/plugins.lua
+++ xdg_base_directory/.config/nvim/lua/user/plugins.lua
@@ -59,6 +59,7 @@ return packer.startup(function(use)
   use { "lewis6991/impatient.nvim", commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6" }
   use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
   use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
+  use { "folke/zen-mode.nvim", commit = "6f5702db4fd4a4c9a212f8de3b7b982f3d93b03c" }
   use {
     "francoiscabrol/ranger.vim",
     commit = "91e82debdf566dfaf47df3aef0a5fd823cedf41c",
