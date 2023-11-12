ce222da5aa5508b96474f5be6fce1509864b6cde
Andrew Sardone <andrew@andrewsardone.com>
2023-11-12 18:32:15 -0500

    Fix Nerd Font glyph issues in NeoVim
    - Upgrade bufferline to fix Nerd Font issue
    - Fix 'Recent Files' Nerd Font glyph
    - Fix broken nerd fonts for completion menu
    
    refs https://github.com/ryanoasis/nerd-fonts/issues/1190

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -51,7 +51,7 @@ return packer.startup(function(use)
   use { "JoosepAlviste/nvim-ts-context-commentstring", commit = "32d9627123321db65a4f158b72b757bcaef1a3f4" }
   use { "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" }
   use { "kyazdani42/nvim-tree.lua", commit = "7282f7de8aedf861fe0162a559fc2b214383c51c" }
-  use { "akinsho/bufferline.nvim", commit = "83bf4dc7bff642e145c8b4547aa596803a8b4dc4" }
+  use { "akinsho/bufferline.nvim", commit = "357cc8f8eeb64702e6fcf2995e3b9becee99a5d3" }
   use { "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" }
   use { "nvim-lualine/lualine.nvim", commit = "a52f078026b27694d2290e34efa61a6e4a690621" }
   use { "akinsho/toggleterm.nvim", commit = "2a787c426ef00cb3488c11b14f5dcf892bbd0bda" }
