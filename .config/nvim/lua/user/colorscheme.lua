8bfd5a400253bd6786a1b095e41d9dd57ef01654
Andrew Sardone <andrew@andrewsardone.com>
2024-03-23 18:01:11 -0400

    Revert automatic dark/light-mode in Neovim and btm
    I just ended up not liking it.
    
    This reverts commit 4d7c83f902c952d660102001a7821f00434be332.
    This reverts commit 46b9b9869bfcc1ef228651c86812bce1535c0413.

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,18 +1,6 @@
 vim.opt.background = "dark"
 local colorscheme = "carbonfox"
 
-local uname = vim.fn.system("uname")
-if string.find(uname, "Darwin") then
-  local interface_style = vim.fn.system("defaults read -g AppleInterfaceStyle 2>&1")
-  if string.find(interface_style, "Dark") then
-    vim.opt.background = "dark"
-    colorscheme = "carbonfox"
-  else
-    vim.opt.background = "light"
-    colorscheme = "dayfox"
-  end
-end
-
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
@@ -26,9 +14,6 @@ if nightfox_status_ok then
       carbonfox = {
         sel0 = "#3F3F3F",
         bg4 = "#FFFFFF",
-      },
-      dayfox = {
-        bg1 = "#FFFFFF",
       }
     },
     options = {
