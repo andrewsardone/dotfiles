9bc5f91678f60c415fc46aacdaf1a5b80bf8c914
Andrew Sardone <andrew@andrewsardone.com>
2025-08-09 22:49:11 -0400

    feat(nvim): disable markdown concealing
    Set `conceallevel` to 0 for Markdown files to show all syntax characters
    instead of hiding them behind conceal formatting.

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable concealing in Markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
